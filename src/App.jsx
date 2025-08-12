9b4f11d3ce7dec9b12add3c74e543a133b83e8a1
Your Name <omolofelix30@gmail.com>
2025-08-12 13:16:08 +0300

    Added a ScrollToTop jsx file to handle routing to external pages accurately

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import DetailsPage from './components/Detailspage';
 import Experience from './components/Experience';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details" element={<DetailsPage />} />
